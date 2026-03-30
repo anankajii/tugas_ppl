5690eee226ca47fb0f7b6de63324eb2cff1198e8
LuszianaE41241960 <lusianaazzahraputri@gmail.com>
2026-03-30 20:27:46 +0700

    memperbaiki perhitungan penjumlahan

--- app.php
+++ app.php
@@ -9,32 +9,37 @@ echo "5. Keluar\n";
 
 $pilih = readline("Pilih: ");
 
-if ($pilih = 1) {
+if ($pilih == 1) {
     $a = readline("Angka 1: ");
     $b = readline("Angka 2: ");
-    echo "Hasil: " . $a + $b . "\n"; 
+    echo "Hasil: " . ($a + $b) . "\n";
 }
+
 elseif ($pilih == 2) {
-    for ($i = 1; $i < 5; $i++) { 
+    for ($i = 1; $i < 5; $i++) {
         echo $i . "\n";
     }
 }
+
 elseif ($pilih == 3) {
     $n = readline("Masukkan angka: ");
-    if ($n % 2 = 0) { 
+    if ($n % 2 == 0) {
         echo "Genap\n";
     } else {
         echo "Ganjil\n";
     }
 }
+
 elseif ($pilih == 4) {
     $s = readline("Sisi: ");
-    $luas = $s * $s + 2;
+    $luas = $s * $s;
     echo "Luas: $luas\n";
 }
+
 elseif ($pilih == 5) {
     echo "Keluar\n";
 }
+
 else {
     echo "Menu salah\n";
 }
